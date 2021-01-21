cf19880cf3a9a75265851e3f2f52399ff0d43494
Nami <nightbird8n@gmail.com>
2021-01-21 17:47:40 +0800

    update

--- AudioRecorder/ViewController.swift
+++ AudioRecorder/ViewController.swift
@@ -6,14 +6,175 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var recorder: AVAudioRecorder!
+    var meterTimer: Timer!
+    var soundFileURL: URL!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "Audio Recording"
+        timeLabel.text = "00:00"
+    }
+    
+    @IBAction func handleRecording(_ sender: Any) {
+        if recorder == nil || !recorder.isRecording {
+            startAudioRecording()
+        } else {
+            stopAudioRecording()
+        }
+    }
+    
+    func startAudioRecording() {
+        if recorder == nil {
+            print("recording. recorder nil")
+            recordWithPermission(true)
+            return
+        }
+        
+        if recorder != nil && recorder.isRecording {
+            print("pausing")
+            recorder.pause()
+            
+        } else {
+            print("recording")
+            recordWithPermission(false)
+        }
+    }
+    
+    func stopAudioRecording() {
+        meterTimer.invalidate()
+        
+        recorder.stop()
+        recorder = nil
+    }
+    
+    @objc func updateAudioMeter(_ timer: Timer) {
+        
+        if let recorder = self.recorder {
+            if recorder.isRecording {
+                let min = Int(recorder.currentTime / 60)
+                let sec = Int(recorder.currentTime.truncatingRemainder(dividingBy: 60))
+                let s = String(format: "%02d:%02d", min, sec)
+                timeLabel.text = s
+                recorder.updateMeters()
+                // if you want to draw some graphics...
+                //var apc0 = recorder.averagePowerForChannel(0)
+                //var peak0 = recorder.peakPowerForChannel(0)
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        recorder = nil
+    }
+    
+    func setupRecorder() {
+        print("\(#function)")
+        
+        self.soundFileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("recording.m4a")
+        print("writing to soundfile url: '\(soundFileURL!)'")
+        
+        if FileManager.default.fileExists(atPath: soundFileURL.absoluteString) {
+            // probably won't happen. want to do something about it?
+            print("soundfile \(soundFileURL.absoluteString) exists")
+        }
+        
+        let recordSettings: [String: Any] = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        
+        do {
+            recorder = try AVAudioRecorder(url: soundFileURL, settings: recordSettings)
+            recorder.delegate = self
+            recorder.isMeteringEnabled = true
+            recorder.prepareToRecord() // creates/overwrites the file at soundFileURL
+        } catch {
+            recorder = nil
+            print("AudioViewController:setupRecorder: \(error.localizedDescription)")
+        }
+        
+    }
+    
+    func recordWithPermission(_ setup: Bool) {
+        print("\(#function)")
+        
+        AVAudioSession.sharedInstance().requestRecordPermission {
+            [unowned self] granted in
+            if granted {
+                
+                DispatchQueue.main.async {
+                    print("Permission to record granted")
+                    self.setSessionPlayAndRecord()
+                    if setup {
+                        self.setupRecorder()
+                    }
+                    self.recorder.record()
+                    
+                    self.meterTimer = Timer.scheduledTimer(timeInterval: 0.01,
+                                                           target: self,
+                                                           selector: #selector(self.updateAudioMeter(_:)),
+                                                           userInfo: nil,
+                                                           repeats: true)
+                }
+            } else {
+                print("Permission to record not granted")
+            }
+        }
+        
+        if AVAudioSession.sharedInstance().recordPermission == .denied {
+            print("permission denied")
+        }
+    }
+    
+    func setSessionPlayAndRecord() {
+        print("\(#function)")
+        
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playAndRecord, mode: .default)
+        } catch {
+            print("could not set session category")
+            print("AudioViewController:setSessionPlayAndRecord:210: \(error.localizedDescription)")
+        }
+        
+        do {
+            try session.setActive(true)
+        } catch {
+            print("could not make session active")
+            print("AudioViewController:setSessionPlayAndRecord:217: \(error.localizedDescription)")
+        }
     }
 
 
 }
 
+extension ViewController: AVAudioRecorderDelegate {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder,
+                                         successfully flag: Bool) {
+        
+        print("\(#function)")
+        
+        print("finished recording \(flag)")
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder,
+                                          error: Error?) {
+        print("\(#function)")
+        
+        if let e = error {
+            print("AudioViewController:audioRecorderEncodeErrorDidOccur: \(e.localizedDescription)")
+        }
+    }
+}
